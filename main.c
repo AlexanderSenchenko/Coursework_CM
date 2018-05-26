063990bb10e6e93426d71c722ebc62d74aab79bf
Antihayp <zayzsacha@mail.ru>
2018-05-26 12:10:42 +0700

    End

--- main.c
+++ main.c
@@ -51,7 +51,6 @@ float ShootingMethod(float x0, float x1, float y0, float y1, float h)
 	float a = 1, b = 0;
 	float fa = 0, fb = 0;
 	float eps = 1E-4;
-	// printf("Sh1\n");
 
 	do {
 		fa = RungeKutt2_time(x0, x1, h, y0, a) - y1;
@@ -59,7 +58,6 @@ float ShootingMethod(float x0, float x1, float y0, float y1, float h)
 		a -= h;
 		b += h;
 	} while (fa * fb > 0);
-	// printf("Sh2\n");
 
 	float g = 0;
 	while(fabs(b - a) > eps) {
@@ -72,8 +70,6 @@ float ShootingMethod(float x0, float x1, float y0, float y1, float h)
 			a = g;
 		}
 	}
-	// printf("Sh3\n");
-
 	return (a + b) / 2;
 }
 
@@ -84,7 +80,7 @@ float DoubleCounting(float x0, float x1, float y0, float y1, float h)
 	float d1[2];
 	int k = 0;
 	for (float h0 = h; delta >= eps; h0 /= 2, k ^= 1) {
-		d1[k] = ShootingMethod(x0, x1, y0, y1, h0);
+		d1[k] = ShootingMethod(x0, x1, y0, y1, h);
 
 		if (h > h0)
 			delta = fabsf(d1[k] - d1[k ^ 1]);
@@ -122,7 +118,6 @@ float* DoubleCountingRunge(float *X, int n, float h, float x0, float x1, float y
 		h1 /= 2;
 
 		if (k == 0) {
-			// printf("Write Y0\n");
 			for (int i = 0; i < n1; i++) {
 				Y0[i] = RungeKutt2_time(x0, X[i], h1, y0, d1);
 				printf("X %f\t", X[i]);
@@ -132,7 +127,6 @@ float* DoubleCountingRunge(float *X, int n, float h, float x0, float x1, float y
 			}
 			printf("\n");
 		} else {
-			// printf("Write Y1\n");
 			for (int i = 0; i < n1; i++) {
 				Y1[i] = RungeKutt2_time(x0, X[i], h1, y0, d1);
 				printf("X %f\t", X[i]);
@@ -214,26 +208,66 @@ float NIntegr(float a, float b, float y0, float d1)
 			float y1 = out_d1;
 			RungeKutt2_time(a, a + (i + 1) * h, h, y0, d1);
 			float y2 = out_d1;
-			S1 += (y1 + y2) / 2;
+			S1 += fabsf(y1 + y2) / 2;
 		}
 
 		S1 *= h;
 		delta = fabsf(S1 - S0);
 		S0 = S1;
-		// printf("%f\n", delta);
 	}
 
 	return S0;
 }
 
+float simpsons_rule(float *XIntegr, float *YIntegr, int n, float h)
+{
+	// // float n0 = n; 
+	// // float n1 = n0;
+	// // float h1 = h;
+
+	// // float S0 = 0;
+	// // float delta = 1;
+	// // float eps = 1E-2;
+
+	// float a = XIntegr[0];
+	// float b = XIntegr[n - 1];
+
+	// // for (;delta >= eps; n1 *= 2) {
+	// // 	float S1 = 0;
+	// 	// h = (b - a) / n1;
+	// 	for (int i = 0, k = 0; i < n; i++, k ^= 1) {
+	// 		if (i == 0 || i == n - 1) {
+	// 			S1 += f(a + i * h);
+	// 		} else if (k == 0) {
+	// 			S1 += f(a + i * h) * 2;
+	// 		} else if (k == 1) {
+	// 			S1 += f(a + i * h) * 4;
+	// 		}
+	// 	}
+	// 	S1 *= h / 3;
+
+	// // 	delta = fabsf(S1 - S0);
+
+	// // 	S0 = S1;
+	// // }
+
+	// return S1;
+
+	float sum = 0;
+	float a = XIntegr[0];
+	float b = XIntegr[n - 1];
+	for(int i = 0; i <= n; i++)
+		sum += (i % 2 ? 4 : 2) * YIntegr[i];
+	return (YIntegr[0] + sum + YIntegr[n - 1]) * h / 3;
+}
+
 int main()
 {
-	float x0 = 0, x1 = 1, y0 = 3, y1 = 3;
+	float x0 = 0, x1 = 1, y0 = 3, y1 = 1;
 	float h = 0.2;
 	int n = 6;
 
-	// float d1 = DoubleCounting(x0, x1, y0, y1, h);
-	float d1 = ShootingMethod(x0, x1, y0, y1, h);
+	float d1 = DoubleCounting(x0, x1, y0, y1, h);
 
 	float *X = malloc(sizeof(float) * n);
 	for (int i = 0; i < n; i++)
@@ -257,8 +291,21 @@ int main()
 	}
 	fclose(out);
 
+	int n2 = 100;
+	float h2 = 1 / n2;
+	float *XIntegr = malloc(sizeof(float) * (n2 + 1));
+	float *YIntegr = malloc(sizeof(float) * (n2 + 1));
+	for (int i = 0; i <= 100; i++) {
+		XIntegr[i] = h2 * i;
+		RungeKutt2_time(x0, XIntegr[i], h2, y0, d1);
+		YIntegr[i] = out_d1;
+	}
+
 	float I = NIntegr(x0, x1, y0, d1);
 	printf("I = %.3f\n", I);
+
+	free(XIntegr);
+	free(YIntegr);
 
 	free(X);
 	free(Y);
